4e7f7d6baf61b35f900d509303fb9788ce3efe05
mcode36 <48412323+mcode36@users.noreply.github.com>
2020-05-17 12:38:42 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,15 +1,12 @@
-$( document ).ready(function() {
-  
-  
-   
+$(document).ready(function() {
+  /*global io*/
+  var socket = io();
+
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function(){
-    var messageToSend = $('#m').val();
+  $("form").submit(function() {
+    var messageToSend = $("#m").val();
     //send message to server here?
-    $('#m').val('');
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
-  
-  
-  
 });
