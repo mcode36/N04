c1fa9a17955cdcb43342d75226e6da294707e5b2
mcode36 <48412323+mcode36@users.noreply.github.com>
2020-05-17 12:38:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
-'use strict';
+"use strict";
+/* 
+Project Page: https://glitch.com/~global-nebulous-verbena
+Live App: https://global-nebulous-verbena.glitch.me
+Stage: Advanced Node and Express - Set up the Environment
+*/
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const auth = require("./app/auth.js");
+const routes = require("./app/routes.js");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const sessionStore = new session.MemoryStore();
 
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: "express.sid",
+    store: sessionStore
+  })
+);
 
+mongo.connect(process.env.DATABASE, (err, client) => {
+  if (err) console.log("Database error: " + err);
+  console.log("Successful database connection");
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
-    auth(app, db);
-    routes(app, db);
-      
-    http.listen(process.env.PORT || 3000);
+  // define database name. This line is required, or mongo will use default satabase name, which is 'admin'
+  const db = client.db("userAuth");
 
-  
-    //start socket.io code  
+  auth(app, db);
+  routes(app, db);
 
-  
+  http.listen(process.env.PORT || 3000);
 
-    //end socket.io code
-  
-  
+  //start socket.io code
+  io.on("connection", socket => {
+    console.log("A user has connected 123");
+  });
+
+  //end socket.io code
 });
